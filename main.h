81c6a52fe172c32a48c53648a130a99fa5205a3a
coderboy-exe <coderboy.exe@gmail.com>
2022-08-10 15:06:46 +0000

    executable

--- main.h
+++ main.h
@@ -7,11 +7,86 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <signal.h>
 
-char **tokenize(char *text, const char *delim, int *count);
-void print_envmnt(void);
+
+/**
+ * struct variables - variables
+ * @av: command line arguments
+ * @buffer: buffer of command
+ * @env: environment variables
+ * @count: count of commands entered
+ * @argv: arguments at opening of shell
+ * @status: exit status
+ * @commands: double pointer to commands
+ */
+
+typedef struct variables
+{
+	char **av;
+	char **argv;
+	char *buffer;
+	char **commands;
+	char **env;
+	size_t count;
+	int status;
+} vars_t;
 
 
+/**
+ * struct builtins - struct for the builtin functions
+ * @name: name of builtin command
+ * @f: function for corresponding builtin
+ */
+typedef struct builtins
+{
+	char *name;
+	void (*f)(vars_t *);
+} builtins_t;
+
+
+/* Environment variable */
+char **create_env(char **env);
+void free_env(char **env);
+
+/* Utility functions */
+ssize_t _puts(char *str);
+char *_strdup(char *strtodup);
+int _strcmpr(char *strcmp1, char *strcmp2);
+char *_strcat(char *strc1, char *strc2);
+unsigned int _strlen(char *str);
+
+/* Tokenization */
+char **tokenize(char *buf, char *delimiter);
+char **_realloc(char **ptr, size_t *size);
+char *_strtok(char *str, const char *delim);
+
+/* Path Handler */
+void check_for_path(vars_t *vars);
+char *find_path(char **env);
+int path_exec(char *command, vars_t *vars);
+int execute_cwd_cmd(vars_t *vars);
+int check_for_dir(char *str);
+
+/* Environment handlers */
+void (*check_for_builtins(vars_t *vars))(vars_t *vars);
+void custom_exit(vars_t *vars);
+void _env(vars_t *vars);
+void _setenv(vars_t *vars);
+void _unsetenv(vars_t *vars);
+
+/* File Finder */
+void add_key(vars_t *vars);
+char **find_key(char **env, char *key);
+char *add_value(char *key, char *value);
+int _atoi(char *str);
+
+/* Error Handling */
+void print_error(vars_t *vars, char *msg);
+void _puts2(char *str);
+char *_uitoa(unsigned int count);
 
 
 #endif
--- shell.c
+++ shell.c
@@ -1,31 +1,68 @@
-#include <unistd.h>
-#include <stdio.h>
+#include "main.h"
+
+/* global variable for ^C handling */
+unsigned int sig_flag;
 
 /**
- * main - entry to main
+ * sig_handler - handles ^C signal interupt
+ * @unused_var: unused variable (required for signal function prototype)
  *
- * @ac: argument counter
- * @av: argument array
- *
- * Return: 0
+ * Return: void
  */
-
-int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
+static void sig_handler(int unused_var)
 {
-	ssize_t read_line;
-	char *lineptr = NULL;
-	size_t n = 0;
-	char *delimiter = " :'\n''\t'";
+	(void) unused_var;
+	if (sig_flag == 0)
+		_puts("\n$ ");
+	else
+		_puts("\n");
+}
 
+/**
+ * main - main function for the shell
+ * @argc: number of arguments passed to main
+ * @argv: array of arguments passed to main
+ * @environment: array of environment variables
+ *
+ * Return: 0 or exit status, or ?
+ */
+int main(int argc __attribute__((unused)), char **argv, char **environment)
+{
+	size_t len_buffer = 0;
+	unsigned int is_pipe = 0, i;
+	vars_t vars = {NULL, NULL, NULL, NULL, NULL, 0, 0};
 
-	while (int_mode)
+	vars.argv = argv;
+	vars.env = create_env(environment);
+	signal(SIGINT, sig_handler);
+	if (!isatty(STDIN_FILENO))
+		is_pipe = 1;
+	if (is_pipe == 0)
+		_puts("$ ");
+	sig_flag = 0;
+	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
 	{
-		int_mode = isatty(STDIN_FILENO);
-
-		if (int_mode == 1)
+		sig_flag = 1;
+		vars.count++;
+		vars.commands = tokenize(vars.buffer, ";");
+		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
 		{
-			write(STDOUT_FILENO, "~$ ", 13);
+			vars.av = tokenize(vars.commands[i], "\n \t\r");
+			if (vars.av && vars.av[0])
+				if (check_for_builtins(&vars) == NULL)
+					check_for_path(&vars);
+			free(vars.av);
 		}
-
+		free(vars.buffer);
+		free(vars.commands);
+		sig_flag = 0;
+		if (is_pipe == 0)
+			_puts("$ ");
+		vars.buffer = NULL;
 	}
+	if (is_pipe == 0)
+		_puts("\n");
+	free_env(vars.env);
+	free(vars.buffer);
+	exit(vars.status);
 }
