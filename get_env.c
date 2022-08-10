56f72fafd9dfd529fa20bdd523e075e4894b4297
coderboy-exe <coderboy.exe@gmail.com>
2022-08-10 15:03:52 +0000

    create and free env

---
+++ get_env.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * create_env - create the shell environment that gets passed to main
+ * @env: environment passed to main
+ *
+ * Return: pointer to new environment
+ */
+
+char **create_env(char **env)
+{
+	char **newenv = NULL;
+	size_t i;
+
+	for (i = 0; env[i] != NULL; i++)
+		;
+	newenv = malloc(sizeof(char *) * (i + 1));
+	if (newenv == NULL)
+	{
+		perror("Fatal Error");
+		exit(1);
+	}
+	for (i = 0; env[i] != NULL; i++)
+		newenv[i] = _strdup(env[i]);
+	newenv[i] = NULL;
+	return (newenv);
+}
+
+/**
+ * free_env - free shell environment
+ * @env: shell environment
+ *
+ * Return: void
+ */
+
+void free_env(char **env)
+{
+	unsigned int i;
+
+	for (i = 0; env[i] != NULL; i++)
+		free(env[i]);
+	free(env);
+}
